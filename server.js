e52c3b9d54933b3e8cb9e48364fa88e7d3d4400d
Shaheer cp <shaheercp890@gmail.com>
2024-09-26 11:26:27 +0530

    routes created

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express =require("express");
 const app = express();
 
+const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin')
 
-app.get('/',(req,res)=>{
-    res.send("hello world..");
-});
+app.use('/user',userRoutes);
+app.use('/admin',adminRoutes);
 
 app.listen(3000,()=>{
-    console.log("server is running...port : 3000");
+    console.log("server is running on : http://localhost:3000/");
 })
