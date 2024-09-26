79cebc943687a0eea16e924e2bb616b00842085f
Shaheer cp <shaheercp890@gmail.com>
2024-09-26 11:51:33 +0530

    view engine set on server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express =require("express");
 const app = express();
 
-const userRoutes = require('./routes/user');
-const adminRoutes = require('./routes/admin')
+const path = require('path');
 
-app.use('/user',userRoutes);
-app.use('/admin',adminRoutes);
+//=============================================
+
+const userRoutes = require('./routes/user');    //specified the path
+const adminRoutes = require('./routes/admin');
+
+
+app.set('views',path.join(__dirname,'views'));   //set view engine
+app.set('view engine','hbs');   
+app.use(express.static('public'));
+
+
+app.use('/user',userRoutes);    //route to user login
+app.use('/admin',adminRoutes);  //route to admin login
 
 app.listen(3000,()=>{
     console.log("server is running on : http://localhost:3000/");
