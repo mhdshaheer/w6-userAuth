d08cb5b7306b7128318736c2bde447666aee24bc
Shaheer cp <shaheercp890@gmail.com>
2024-09-30 16:11:11 +0530

    login page and home are done

--- controller/userController.js
+++ controller/userController.js
@@ -1,56 +1,63 @@
 const userSchema = require('../model/userModel');
+const bcrypt = require('bcrypt');
+const saltround = 10;
 
-const registerUser = async (req,res)=>{
+const registerUser = async (req, res) => {
     try {
-        const {username,password} = req.body;
+        const { username, password } = req.body;
 
-        const user = await userSchema.findOne({username});
+        const user = await userSchema.findOne({ username });
 
-        if(user){
-            return res.render('user/register',{message:'Username is already exist'})
+        if (user) {
+            return res.render('user/register', { message: 'Username is already exist' })
         }
- 
+        const hashedPassword = await bcrypt.hash(password, saltround);
         const newUser = new userSchema({
             username,
-            password,
+            password: hashedPassword,
         });
         await newUser.save();
-        res.render('user/login',{message:'Username is created succesfully'});
+        res.render('user/login', { message: 'Username is created succesfully' });
 
 
     } catch (error) {
-
+        res.render('user/register', { message: 'Something went wrong' })
     }
 }
 
-const login = async (req,res)=>{
+const login = async (req, res) => {
     try {
-        const {username,password} =req.body;
+        const { username, password } = req.body;
         const user = await userSchema.findOne({username});
+        
 
-        if(!user){
-            return res.render('user/login',{message:'User does not exist'});
+        if (!user) {
+            return res.render('user/login', { message: 'User does not exist' });
 
         }
-        if(user.password !== password ){
-            return res.render('user/login',{message:'Incorrect password'})
-        }
-        res.render('user/home',{message:'login successful'})
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) res.render('user/login',{message:'password incorrect'})
+        res.render('user/home');
 
     } catch (error) {
-        
+        res.render('user/login', { message: 'Something went wrong' })
     }
 }
 
-const loadLogin = (req,res)=>{
+const loadLogin = (req, res) => {
     res.render('user/login');
 };
-const loadRegister = (req,res) =>{
+const loadRegister = (req, res) => {
     res.render('user/register')
+};
+const loadHome = (req,res)=>{
+    res.render('user/home');
 }
 
 module.exports = {
     registerUser,
+    login,
     loadLogin,
     loadRegister,
+    loadHome
 }
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,9 @@ const userController = require('../controller/userController');
 
 
 route.get('/login',userController.loadLogin);
+route.post('/login',userController.login);
 route.get('/register',userController.loadRegister);
-route.post('/register',userController.registerUser)
+route.post('/register',userController.registerUser);
+route.get('home',userController.loadHome);
 
 module.exports = route;
