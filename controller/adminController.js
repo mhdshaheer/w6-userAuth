52d5e7dbdea144408c5a3938171f44d1513a94ac
Shaheer cp <shaheercp890@gmail.com>
2024-10-04 17:42:05 +0530

    admin dashboard is finished'

--- controller/adminController.js
+++ controller/adminController.js
@@ -3,25 +3,25 @@ const bcrypt = require('bcrypt');
 const userModel = require('../model/userModel')
 
 
-const loadLogin = async (req,res)=>{
+const loadLogin = async (req, res) => {
     res.render('admin/login')
 }
-const login =async (req,res)=>{
+const login = async (req, res) => {
     try {
-       const {username,password} = req.body;
-       
-       const admin = await adminModel.findOne({username});
+        const { username, password } = req.body;
 
-       if(!admin){
-        return res.render('admin/login',{message:'Invalid credentials'});
-       }
-       const isMatch = await bcrypt.compare(password,admin.password);
-       console.log(isMatch,password,admin.password)
+        const admin = await adminModel.findOne({ username });
 
-       if(!isMatch) return res.render('admin/login',{message : 'Incorrect password'});
-       req.session.admin = true;
-       req.session.adminMessage = 'success'
-       res.redirect('/admin/dashboard')
+        if (!admin) {
+            return res.render('admin/login', { message: 'Invalid credentials' });
+        }
+        const isMatch = await bcrypt.compare(password, admin.password);
+        console.log(isMatch, password, admin.password)
+
+        if (!isMatch) return res.render('admin/login', { message: 'Incorrect password' });
+        req.session.admin = true;
+        req.session.adminMessage = 'success'
+        res.redirect('/admin/dashboard')
 
 
     } catch (error) {
@@ -29,45 +29,92 @@ const login =async (req,res)=>{
     }
 }
 
-const LoadDashboard = async (req,res)=>{
+const LoadDashboard = async (req, res) => {
     try {
-        const admin =req.session.admin;
-        
-        
-        if(!admin) return res.redirect('/admin/login');
+        const admin = req.session.admin;
+
+
+        if (!admin) return res.redirect('/admin/login');
 
         const users = await userModel.find({})
         console.log(users)
         let message = 'Login successful'
-         res.render('admin/dashboard', { users, adminMessage:req.session.adminMessage, message});
-         if(req.session.adminMessage){
+        res.render('admin/dashboard', { users, adminMessage: req.session.adminMessage, message });
+        if (req.session.adminMessage) {
             message = ' '
             req.session.adminMessage = null
-         }
+        }
 
-      
+
 
     } catch (error) {
         res.render('admin/login', { message: 'Something went wrong' })
     }
 }
 
-const editUser = async (req,res)=>{
+const editUser = async (req, res) => {
     try {
-        
-        const {username,password} = req.body;
-        const user = await userModel.findOneAndUpdate({_id:id},{username},{password});
-        console.log(user);
-        res.json(user)
-        
+
+        const { username, password, id } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        // const user = await userModel.findOneAndUpdate({_id:id},{username},{password});
+        const user = await userModel.findOneAndUpdate({ _id: id }, { $set: { username, password: hashedPassword } });
+
+        res.redirect('/admin/dashboard')
+
 
     } catch (error) {
         console.log(error)
     }
 }
+
+const deleteUser = async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const user = await userModel.findOneAndDelete({_id:id});
+
+        res.redirect('/admin/dashboard')
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const addUser = async (req,res) =>{
+    try {
+        
+        const {username,password} = req.body;
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const newUser = new userModel({
+            username,
+            password:hashedPassword
+        })
+
+        await newUser.save()
+        res.redirect('/admin/dashboard');
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+const logout = async (req,res) =>{
+    req.session.admin = null;
+    res.redirect('/admin/login')
+}
+
+
+
 module.exports = {
     loadLogin,
     login,
     LoadDashboard,
-    editUser
+    editUser,
+    deleteUser,
+    addUser,
+    logout
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,11 @@ const adminController = require('../controller/adminController')
 const adminAuth = require('../middleware/adminAuth')
 
 router.get('/login',adminAuth.isLogin,adminController.loadLogin);
-router.post('/login',adminController.login)
+router.post('/login',adminController.login);
 router.get('/dashboard',adminAuth.checkSession,adminController.LoadDashboard);
-router.post('/edit-user',adminAuth.checkSession,adminController.editUser)
+router.post('/edit-user',adminAuth.checkSession,adminController.editUser);
+router.get('/delete-user/:id',adminAuth.checkSession,adminController.deleteUser);
+router.post('/add-user',adminAuth.checkSession,adminController.addUser);
+router.get('/logout',adminAuth.checkSession,adminController.logout);
 
 module.exports = router;
